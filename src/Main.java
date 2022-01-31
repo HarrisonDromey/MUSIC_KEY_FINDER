574c55757c7e91b17dda6802f719b6a0b5c88452
HarrisonDromey <harrisondromey@gmail.com>
2022-01-31 18:20:43 -0500

    session one

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        //My first comment
-        System.out.println("Hello World!!!");
+    public static void main(String[] args) {
+
     }
 }
